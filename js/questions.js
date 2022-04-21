27a72bab0ee11b70acc4eb11809c2403fab43cff
naitikkkkkk <98087490+naitikkkkkk@users.noreply.github.com>
2022-04-21 17:34:10 +0530

    Earth Day

---
+++ js/questions.js
@@ -0,0 +1,117 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Immersing Yourself Mindfully In Nature Is Known In Japan As 'Shinrin-Yoku'. What Does This Lterally Mean?",
+    answer: "Forest Bathing",
+    options: [
+      "Forest Bathing",
+      "Going Green",
+      "Returning To Your Roots",
+      "enchanting"
+    ]
+  },
+    {
+    numb: 2,
+    question: "It's Loved To Recycle Plastic That's Dirty,Low Value Or Mis-Sorted .So,Even Through Many Of Us Out Over Plastic Waste In The Blue Bin,What Percentage Of It Actually Gets Recycled?S",
+    answer: "Around 9%",
+    options: [
+      "Around 30%",
+      "Around 50%",
+      "Around 9%",
+      "Around 22%"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Globally,2020 Was The Hottedt Year So Far On Record?",
+    answer: "True",
+    options: [
+    "True",
+      "False"
+    ]
+  },
+    {
+    numb: 4,
+    question: "So,Far How Many Mass Exinction Events Have There Been In Earth's History?",
+    answer: "Five",
+    options: [
+      "One",
+      "None",
+      "Five",
+      "Ten"
+    ]
+  },
+    {
+    numb: 5,
+    question: "In What Year Did The First Earth Take Place?",
+    answer: "1970",
+    options: [
+      "1970",
+      "1980",
+      "1996",
+      "1974"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who Is Called The Father Of Earth Day?",
+    answer: "Gaylord Nelson",
+    options: [
+      "Wangari Maathai",
+      "Mark Boyle",
+      "Vandana Shiva",
+      "Gaylord Nelson"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which Is A More Appropriate Reason To Observe The Earth Day Every Yesr?",
+    answer: "To Support For Environmental Protection",
+    options: [
+      "To Save The Ozone Layer",
+      "To Protect The Species",
+      "To Support For Environmental Protection",
+      "None Of The Above"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "How Much Oxygen Does An Average 50 Year Old Tree Provides?",
+    answer: "Enough For 4 Person",
+    options: [
+      "Enough For 4 Person",
+      "Enough For 1 Person",
+      "Enough For 8 Person",
+      "Enough For 10 Person"
+    ]
+  },
+
+
+  {
+    numb: 9,
+    question: "What Was The Purpose Of The National Environmental Education Act Passed By Congress In 1990?",
+    answer: "To require that the united states environmental protection agency be a leader in promoting environmental literacy.",
+    options: [
+      "To establish the office of environmental education.",
+      "To require that the united states environmental protection agency be a leader in promoting environmental literacy.",
+      "Both a&b",
+      "None Of the above"
+    ]
+  },
+
+
+  {
+    numb: 10,
+    question: "Which Land Based Ecosystem Holds the most biodiversity i.e. concentration of plants and animals?",
+    answer: "Tropical Forest",
+    options: [
+      "South Pacific Island",
+      "African Savannah",
+      "Tropical Forest",
+      "Coral Reefs"
+    ]
+  },
+
+];
